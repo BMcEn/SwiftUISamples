381574af7b15115691729710908e38768c3a1c25
Ben McEnulty <30420560+BMcEn@users.noreply.github.com>
2021-11-02 19:59:45 -0700

    WatchOS target added

---
+++ SwiftUIWatchSamples WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIWatchSamples WatchKit Extension
+//
+//  Created by Benjamin McEnulty on 11/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIWatchSamples WatchKit Extension/SwiftUISamplesApp.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUISamplesApp.swift
+//  SwiftUIWatchSamples WatchKit Extension
+//
+//  Created by Benjamin McEnulty on 11/2/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
