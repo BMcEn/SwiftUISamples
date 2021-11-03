1bf40ad960bb1de9fa56ddbb62b44a40c916cfb8
Ben McEnulty <30420560+BMcEn@users.noreply.github.com>
2021-11-02 17:08:38 -0700

    Initial Commit

---
+++ SwiftUISamples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUISamples
+//
+//  Created by Benjamin McEnulty on 11/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUISamples/SwiftUISamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISamplesApp.swift
+//  SwiftUISamples
+//
+//  Created by Benjamin McEnulty on 11/2/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
